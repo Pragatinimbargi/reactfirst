b5452478036af38d1103b0f00c3ded1935b83658
Pragatinimbargi <120628440+Pragatinimbargi@users.noreply.github.com>
2023-03-09 14:27:05 +0530

    Add files via upload

---
+++ Student.jsx
@@ -0,0 +1,171 @@
+import React from 'react'
+const Student=(props)=>{
+    console.log(props);
+    return(
+        <div className='Parentblock'>
+            
+            <div className="mainblock">
+                <img src={props.payload[0].Studentphoto} alt=""/>
+                <h2>{props.payload[0].StudentName}</h2>
+                <h3>{props.payload[0].Id}</h3>
+                <h3>{props.payload[0].Branch}</h3>
+                <h3>{props.payload[0].YOP}</h3>
+                <h4>{props.payload[0].Rollno}</h4>
+            </div>
+            <div className='mainblock'>
+                <img src={props.payload[1].Studentphoto} alt=""/>
+                <h2>{props.payload[1].StudentName}</h2>
+                <h3>{props.payload[1].Id}</h3>
+                <h3>{props.payload[1].Branch}</h3>
+                <h3>{props.payload[1].YOP}</h3>
+                <h4>{props.payload[1].Rollno}</h4>
+            </div>
+            <div className='mainblock'>
+                <img src={props.payload[2].Studentphoto} alt=""/>
+                <h2>{props.payload[2].StudentName}</h2>
+                <h3>{props.payload[2].Id}</h3>
+                <h3>{props.payload[2].Branch}</h3>
+                <h3>{props.payload[2].YOP}</h3>
+                <h4>{props.payload[2].Rollno}</h4>
+            </div>
+            <div className='mainblock'>
+                <img src={props.payload[3].Studentphoto} alt=""/>
+                <h2>{props.payload[3].StudentName}</h2>
+                <h3>{props.payload[3].Id}</h3>
+                <h3>{props.payload[3].Branch}</h3>
+                <h3>{props.payload[3].YOP}</h3>
+                <h4>{props.payload[3].Rollno}</h4>
+            </div>
+            <div className='mainblock'>
+                <img src={props.payload[4].Studentphoto } alt=""/>
+                <h2>{props.payload[4].StudentName}</h2>
+                <h3>{props.payload[4].Id}</h3>
+                <h3>{props.payload[4].Branch}</h3>
+                <h3>{props.payload[4].YOP}</h3>
+                <h4>{props.payload[4].Rollno}</h4>
+            </div><br></br>
+            <div className='mainblock'>
+                <img src={props.payload[5].Studentphoto} alt=""/>
+                <h2>{props.payload[5].StudentName}</h2>
+                <h3>{props.payload[5].Id}</h3>
+                <h3>{props.payload[5].Branch}</h3>
+                <h3>{props.payload[5].YOP}</h3>
+                <h4>{props.payload[5].Rollno}</h4>
+            </div>
+            <div className='mainblock'>
+                <img src={props.payload[6].Studentphoto }alt=""/>
+                <h2>{props.payload[6].StudentName}</h2>
+                <h3>{props.payload[6].Id}</h3>
+                <h3>{props.payload[6].Branch}</h3>
+                <h3>{props.payload[6].YOP}</h3>
+                <h4>{props.payload[6].Rollno}</h4>
+            </div>
+            <div className='mainblock'>
+                <img src={props.payload[7].Studentphoto} alt=""/>
+                <h2>{props.payload[7].StudentName}</h2>
+                <h3>{props.payload[7].Id}</h3>
+                <h3>{props.payload[7].Branch}</h3>
+                <h3>{props.payload[7].YOP}</h3>
+                <h4>{props.payload[7].Rollno}</h4>
+            </div>
+            <div className='mainblock'>
+                <img src={props.payload[8].Studentphoto }alt=""/>
+                <h2>{props.payload[8].StudentName}</h2>
+                <h3>{props.payload[8].Id}</h3>
+                <h3>{props.payload[8].Branch}</h3>
+                <h3>{props.payload[8].YOP}</h3>
+                <h4>{props.payload[8].Rollno}</h4>
+            </div>
+            <div className='mainblock'>
+                <img src={props.payload[9].Studentphoto} alt=""/>
+                <h2>{props.payload[9].StudentName}</h2>
+                <h3>{props.payload[9].Id}</h3>
+                <h3>{props.payload[9].Branch}</h3>
+                <h3>{props.payload[9].YOP}</h3>
+                <h4>{props.payload[9].Rollno}</h4>
+            </div>
+          
+            <div className='mainblock'>
+                <img src={props.payload[10].Studentphoto} alt=""/>
+                <h2>{props.payload[10].StudentName}</h2>
+                <h3>{props.payload[10].Id}</h3>
+                <h3>{props.payload[10].Branch}</h3>
+                <h3>{props.payload[10].YOP}</h3>
+                <h4>{props.payload[10].Rollno}</h4>
+            </div>
+            <div className='mainblock'>
+                <img src={props.payload[11].Studentphoto }alt=""/>
+                <h2>{props.payload[11].StudentName}</h2>
+                <h3>{props.payload[11].Id}</h3>
+                <h3>{props.payload[11].Branch}</h3>
+                <h3>{props.payload[11].YOP}</h3>
+                <h4>{props.payload[11].Rollno}</h4>
+            </div>
+            <div className='mainblock'>
+                <img src={props.payload[12].Studentphoto }alt=""/>
+                <h2>{props.payload[12].StudentName}</h2>
+                <h3>{props.payload[12].Id}</h3>
+                <h3>{props.payload[12].Branch}</h3>
+                <h3>{props.payload[12].YOP}</h3>
+                <h4>{props.payload[12].Rollno}</h4>
+            </div>
+            <div className='mainblock'>
+                <img src={props.payload[13].Studentphoto} alt=""/>
+                <h2>{props.payload[13].StudentName}</h2>
+                <h3>{props.payload[13].Id}</h3>
+                <h3>{props.payload[13].Branch}</h3>
+                <h3>{props.payload[13].YOP}</h3>
+                <h4>{props.payload[13].Rollno}</h4>
+            </div>
+            <div className='mainblock'>
+                <img src={props.payload[14].Studentphoto} alt=""/>
+                <h2>{props.payload[14].StudentName}</h2>
+                <h3>{props.payload[14].Id}</h3>
+                <h3>{props.payload[14].Branch}</h3>
+                <h3>{props.payload[14].YOP}</h3>
+                <h4>{props.payload[14].Rollno}</h4>
+            </div>
+            <div className='mainblock'>
+                <img src={props.payload[15].Studentphoto} alt=""/>
+                <h2>{props.payload[15].StudentName}</h2>
+                <h3>{props.payload[15].Id}</h3>
+                <h3>{props.payload[15].Branch}</h3>
+                <h3>{props.payload[15].YOP}</h3>
+                <h4>{props.payload[15].Rollno}</h4>
+            </div>
+            <div className='mainblock'>
+                <img src={props.payload[16].Studentphoto} alt=""/>
+                <h2>{props.payload[16].StudentName}</h2>
+                <h3>{props.payload[16].Id}</h3>
+                <h3>{props.payload[16].Branch}</h3>
+                <h3>{props.payload[16].YOP}</h3>
+                <h4>{props.payload[16].Rollno}</h4>
+            </div>
+            <div className='mainblock'>
+                <img src={props.payload[17].Studentphoto }alt=""/>
+                <h2>{props.payload[17].StudentName}</h2>
+                <h3>{props.payload[17].Id}</h3>
+                <h3>{props.payload[17].Branch}</h3>
+                <h3>{props.payload[17].YOP}</h3>
+                <h4>{props.payload[17].Rollno}</h4>
+            </div>
+            <div className='mainblock'>
+                <img src={props.payload[18].Studentphoto }alt=""/>
+                <h2>{props.payload[18].StudentName}</h2>
+                <h3>{props.payload[18].Id}</h3>
+                <h3>{props.payload[18].Branch}</h3>
+                <h3>{props.payload[18].YOP}</h3>
+                <h4>{props.payload[18].Rollno}</h4>
+            </div>
+            <div className='mainblock'>
+                <img src={props.payload[19].Studentphoto} alt=""/>
+                <h2>{props.payload[19].StudentName}</h2>
+                <h3>{props.payload[19].Id}</h3>
+                <h3>{props.payload[19].Branch}</h3>
+                <h3>{props.payload[19].YOP}</h3>
+                <h4>{props.payload[19].Rollno}</h4>
+            </div>
+        </div>
+    )
+}
+export default Student
